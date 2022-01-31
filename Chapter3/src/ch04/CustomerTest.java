a2e7f8a72d09d55d97e9f89e8e7188d7a075fa80
seongyeong826 <37947951+seongyeong826@users.noreply.github.com>
2022-02-01 00:57:08 +0900

    Chapter3 - 04. 메서드 재정의 하기(overriding)

---
+++ Chapter3/src/ch04/CustomerTest.java
@@ -0,0 +1,31 @@
+package ch04;
+
+public class CustomerTest {
+	
+	public static void main(String[] args) {
+		
+//		Customer customerLee = new Customer();
+//		customerLee.setCustomerName("이순신");
+//		customerLee.setCustomerID(10010);
+		
+		Customer customerLee = new Customer(1010, "이순신");
+		customerLee.bonusPoint = 1000;
+		int price = customerLee.calcPrice(1000);
+		
+		System.out.println(customerLee.showCustomerInfo() + price);
+		
+		
+		
+//		VIPCustomer customerKim = new VIPCustomer();
+//		customerKim.setCustomerName("김유신");
+//		customerKim.setCustomerID(10020);
+		
+		VIPCustomer customerKim = new VIPCustomer(10020, "김유신");
+		customerKim.bonusPoint = 10000;
+		price = customerKim.calcPrice(1000);
+		
+		System.out.println(customerKim.showCustomerInfo() + price);
+		
+	}
+
+}
