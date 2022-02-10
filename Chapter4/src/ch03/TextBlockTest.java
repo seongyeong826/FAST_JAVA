51523c325d4771b78bf4f37b451a0d29fef0ada4
seongyeong826 <37947951+seongyeong826@users.noreply.github.com>
2022-02-11 01:09:50 +0900

    Chapter4 - 03. String, StringBuilder, StringBuffer 클래스, text block

---
+++ Chapter4/src/ch03/TextBlockTest.java
@@ -0,0 +1,27 @@
+package ch03;
+
+public class TextBlockTest {
+
+	public static void main(String[] args) {
+		
+//		String textBlocks = """
+//				Hello,
+//				Him,
+//				how r u""";
+//		System.out.println(textBlocks);
+//		System.out.println(getBlockOfHtml);
+
+	}
+	
+//	public static String getBlockOfHtml() {
+//		return """
+//				<html>
+//					<body>
+//						<span>example text</span>
+//					</body>
+//				</html> """;
+//	}
+	
+	// java13 부터 제공되는 내용
+
+}
